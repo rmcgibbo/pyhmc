c81aee2c2dc522d1187a1e64d0e277e01414c030
Robert McGibbon <rmcgibbo@gmail.com>
2014-12-21 03:14:21 -0800

    make test run

--- pyhmc/autocorr2.py
+++ pyhmc/autocorr2.py
@@ -1,23 +1,6 @@
 import numpy as np
 from scipy.linalg import toeplitz
 
-# for testing against R
-from rpy2.robjects import r
-import rpy2.robjects.numpy2ri
-rpy2.robjects.numpy2ri.activate()
-
-from .hmc import _check_random_state
-
-
-def generate_AR1(phi, sigma, n_steps, c=0, y0=0, random_state=None):
-    y = np.zeros(n_steps)
-    y[0] = y0
-    random = _check_random_state(random_state)
-    rand = random.normal(scale=sigma, size=(n_steps,))
-    for i in range(1, n_steps):
-        y[i] = c + phi*y[i-1] + rand[i]
-    return y
-
 
 def yule_walker(X, aic=True, order_max=None, demean=True):
     """Estimate AR(p) parameters from a sequence X using Yule-Walker equation.
@@ -100,13 +83,3 @@ def integrated_autocorr(x):
     tau_int = spec0 / np.var(x, ddof=1)
     return tau_int
 
-
-def test_1():
-    r("require('coda')")
-
-    random = np.random.RandomState(1)
-    for i in range(10):
-        x = generate_AR1(phi=0.95, sigma=1, n_steps=1000, c=0, y0=0, random_state=random)
-        r.assign('x', x)
-        tau = r('nrow(x)/effectiveSize(x)')[0]
-        np.testing.assert_approx_equal(tau, integrated_autocorr(x))
